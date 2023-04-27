195b5c6843c4f9239d5190626891741ea1bc160b
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-04-27 13:46:04 +0300

    Removes unnecessary line in JS

--- scripts.js
+++ scripts.js
@@ -1,8 +1,6 @@
 const navMain = document.querySelector('.navbar');
 const navToggle = document.querySelector('.main-nav-toggle');
 
-navMain.classList.remove('main-nav--nojs');
-
 navToggle.addEventListener('click', function () {
   navMain.classList.toggle('navbar-opened');
   navToggle.classList.toggle('main-nav-toggle-opened');
